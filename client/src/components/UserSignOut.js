0fda21327b810fd223aecdbe732b6874c0a82b30
Manoel Blue <manoelbluee@gmail.com>
2021-04-16 13:30:55 +0200

    add redirect to userSignOut

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 
 const UserSignOut = () => {
     return (
         // Redirect to other route
         <div>
-            
+            <Redirect to="/"></Redirect>
         </div>
     )
 }
