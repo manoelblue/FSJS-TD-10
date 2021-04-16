f039e279718ea4919032e31f9be53b4d07ae58cc
Manoel Blue <manoelbluee@gmail.com>
2021-04-16 16:06:13 +0200

    include form component in CreateCourse

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,54 +1,46 @@
-// import React, {useState} from 'react';
+import React, {Component} from 'react';
+import Form from './Form';
 
-const CreateCourse = () => {
-    // States:
-    // const [title, setTitle] = useState("");
-    // const [description, setDescription] = useState("");
-    // const [estimatedTime, setTime] = useState("");
-    // const [materialsNeeded, setMaterials] = useState("");
-    // const [author, setAuthor] = useState("");
+class CreateCourse extends Component {
+    state = {
+        courseTitle: "",
+        courseAuthor: "",
+        courseDescription: "",
+        estimatedTime: "",
+        materialsNeeded: "",
+    }
 
-    // Methods:
-    // const submit = () => {
+    render() {
+        const {courseTitle, courseAuthor, courseDescription, estimatedTime, materialsNeeded} = this.state;
 
-    // }
-
-    return (
-        <main>
-            <div className="wrap">
-                <h2>Create Course</h2>
-                <div className="validation--errors">
-                    <h3>Validation Errors</h3>
-                    <ul>
-                        <li>Please provide a value for "Title"</li>
-                        <li>Please provide a value for "Description"</li>
-                    </ul>
-                </div>
-                <form>
-                    <div className="main--flex">
-                        <div>
-                            <label for="courseTitle">Course Title</label>
-                            <input id="courseTitle" name="courseTitle" type="text" value="" />
-
-                            <label for="courseAuthor">Course Author</label>
-                            <input id="courseAuthor" name="courseAuthor" type="text" value="Joe Smith" />
-
-                            <label for="courseDescription">Course Description</label>
-                            <textarea id="courseDescription" name="courseDescription"></textarea>
-                        </div>
-                        <div>
-                            <label for="estimatedTime">Estimated Time</label>
-                            <input id="estimatedTime" name="estimatedTime" type="text" value="" />
-
-                            <label for="materialsNeeded">Materials Needed</label>
-                            <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
-                        </div>
-                    </div>
-                    <button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
-                </form>
-            </div>
-        </main>
-    )
+        return (
+            <Form
+                cancel={this.cancel}
+                submit={this.submit}
+                errors={errors}
+                submiButtonText="Sign In"
+                elements={() => (
+                    <React.Fragment>
+                        <label htmlFor="courseTitle">Course Title</label>
+                        <input
+                            id="courseTitle" 
+                            name="courseTitle"
+                            type="text"
+                            value={courseTitle} 
+                            onChange={this.change} 
+                            placeholder="Course Title" />
+                        <label htmlFor="courseAuthor">Course Author</label>
+                        <input
+                            id="courseAuthor" 
+                            name="courseAuthor" 
+                            type="text"
+                            value={courseAuthor} 
+                            onChange={this.change} 
+                            placeholder="Course Author" />
+                    </React.Fragment>
+                )}/>
+        )
+    }
 };
 
 export default CreateCourse;
