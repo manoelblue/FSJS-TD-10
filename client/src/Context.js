89660a59574b030e9131158522ef88667f10102a
Manoel Blue <manoelbluee@gmail.com>
2021-04-20 09:14:48 +0200

    add cookie and authUser to context

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import Cookies from 'js-cookie';
 import Data from './Data';
 
 const Context = React.createContext(); 
 
 export class Provider extends Component {
+
+    state = {
+        authenticatedUser: Cookies.getJSON('authenticatedUser') || null
+    };
+
     constructor() {
         super();
         this.data = new Data();
     }
 
     render() {
+        const { authenticatedUser } = this.state;
         const value = {
+            authenticatedUser,
             data: this.data,
             actions: {
                 signIn: this.signIn,
@@ -18,27 +26,31 @@ export class Provider extends Component {
             },
         };
         return (
-        <Context.Provider value={value}>
-            {this.props.children}
-        </Context.Provider>  
+            <Context.Provider value={value}>
+                {this.props.children}
+            </Context.Provider>  
         );
     }
-
 
     signIn = async (username, password) => {
         const user = await this.data.getUser(username, password);
         if (user !== null) {
             this.setState(() => {
                 return {
-                    authenticatedUser: user,
+                authenticatedUser: user,
                 };
             });
+            const cookieOptions = {
+                expires: 1 // 1 day
+            };
+            Cookies.set('authenticatedUser', JSON.stringify(user), {cookieOptions});
         }
         return user;
     }
 
     signOut = () => {
         this.setState({ authenticatedUser: null });
+        Cookies.remove('authenticatedUser');
     }
 }
 
@@ -53,9 +65,9 @@ export const Consumer = Context.Consumer;
 export default function withContext(Component) {
     return function ContextComponent(props) {
         return (
-        <Context.Consumer>
-            {context => <Component {...props} context={context} />}
-        </Context.Consumer>
+            <Context.Consumer>
+                {context => <Component {...props} context={context} />}
+            </Context.Consumer>
         );
     }
 }
