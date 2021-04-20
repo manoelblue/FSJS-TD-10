3420a253e958813a3f36edf4a4292e52ef57d344
Manoel Blue <manoelbluee@gmail.com>
2021-04-20 16:23:33 +0200

    fix rendering of course details

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -14,55 +14,59 @@ const CourseDetail = (props) => {
         fetch(`${config.apiBaseUrl}/courses/${id}`)
         .then(res => res.json())
         .then(data => setCourse(data[0]))
-        .catch(error => {
+                .catch(error => {
             <Redirect error={error} to="/error" />
         })
     }, [id]);
 
     const {title, description, estimatedTime, materialsNeeded} = course;
 
-    course ? (
-        <React.Fragment>
-            <div className="actions--bar">
-                <div className="wrap">
-                    { authUser && authUser.id === course.userId ?
-                        <React.Fragment>
-                            <Link className="button" to="/courses/:id/update">Update Course</Link>
-                            <Link className="button" to="/">Delete Course</Link>
-                        </React.Fragment>
-                        : null
-                    }
-                    <Link className="button button-secondary" to="/">Return to List</Link>
-                </div>
-            </div>
-            
-            <div className="wrap">
-                <h2>Course Detail</h2>
-                <form>
-                    <div className="main--flex">
-                        <div>
-                            <h3 className="course--detail--title">Course</h3>
-                            <h4 className="course--name">{title}</h4>
-                            <p>By Joe Smith</p>
-
-                            <p><ReactMarkdown>{description}</ReactMarkdown></p>
-                        </div>
-                        <div>
-                            <h3 className="course--detail--title">Estimated Time</h3>
-                            <p>{estimatedTime}</p>
-
-                            <h3 className="course--detail--title">Materials Needed</h3>
-                            <ul className="course--detail--list">
-                                <ReactMarkdown>{materialsNeeded}</ReactMarkdown>
-                            </ul>
-                        </div>
+    if (course) {
+        return (
+            <React.Fragment>
+                <div className="actions--bar">
+                    <div className="wrap">
+                        { authUser && authUser.id === course.userId ?
+                            <React.Fragment>
+                                <Link className="button" to="/courses/:id/update">Update Course</Link>
+                                <Link className="button" to="/">Delete Course</Link>
+                            </React.Fragment>
+                            : null
+                        }
+                        <Link className="button button-secondary" to="/">Return to List</Link>
                     </div>
-                </form>
-            </div>
-        </React.Fragment>
-    ) : (
-        <Redirect to="/notfound" />
-    )
+                </div>
+                
+                <div className="wrap">
+                    <h2>Course Detail</h2>
+                    <form>
+                        <div className="main--flex">
+                            <div>
+                                <h3 className="course--detail--title">Course</h3>
+                                <h4 className="course--name">{title}</h4>
+                                <p>By Joe Smith</p>
+
+                                <ReactMarkdown>{description}</ReactMarkdown>
+                            </div>
+                            <div>
+                                <h3 className="course--detail--title">Estimated Time</h3>
+                                <p>{estimatedTime}</p>
+
+                                <h3 className="course--detail--title">Materials Needed</h3>
+                                <ul className="course--detail--list">
+                                    <ReactMarkdown>{materialsNeeded}</ReactMarkdown>
+                                </ul>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </React.Fragment>
+        )
+    } else {
+        return (
+            <Redirect to="/notfound" />
+        )
+    }
 
 }
 
