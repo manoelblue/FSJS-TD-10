d0fb557fea82ab11cbd0965b7e01a4c503cc1d41
Manoel Blue <manoelbluee@gmail.com>
2021-04-20 15:42:29 +0200

    add friendly messages components

---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forbidden = () => {
+    return (
+        <div className="wrap">
+            <h2>Forbidden Page!</h2>
+            <p>Sorry! The page you are requesting cannot be accessed.</p>
+        </div>
+    )
+}
+
+export default Forbidden;
---
+++ client/src/components/UnhandleError.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UnhandleError = () => {
+    return (
+        <div className="wrap">
+            <h2>Unhandle Error!</h2>
+            <p>Sorry! An unexpected error has occurred.</p>
+        </div>
+    )
+}
+
+export default UnhandleError;
