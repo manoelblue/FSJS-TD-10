b5e5720cf5364442c5e6ff1f16ed2d998a644e31
Manoel Blue <manoelbluee@gmail.com>
2021-04-30 15:45:48 +0200

    create history

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Router, Switch, Route} from 'react-router-dom';
+import history from './history';
 
 // Context
 import withContext from './Context';
@@ -30,7 +31,7 @@ const UserSignOutWithContext =  withContext(UserSignOut);
 
 const App = () => {
   return (
-    <Router>
+    <Router history={history}>
         <HeaderWithContext />
 
         <main>
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,50 +1,59 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import {Link, Redirect} from 'react-router-dom';
 
 import config from '../config';
 
-const Courses = () => {
-    const [courses, setCourses] = useState([]);
+class Courses extends Component {
+    state = {
+        courses: {}
+    }
 
     // Fetch courses:
-    useEffect(() => {
+    componentDidMount() {
         fetch(`${config.apiBaseUrl}/courses`)
             .then(res => res.json())
-            .then(data => setCourses(data))
+            .then(data => this.setState({
+                courses: data,
+            }))
             .catch(error => {
                 <Redirect error={error} to="/error" />
             })
-    }, []);
+    }
 
-    // Map over courses
-    const coursesList = courses.map((course, index) => (
-        <Link
-            to={`courses/${course.id}`}
-            key={index}
-            className="course--module course--link"
-        >
-            <h2 className="course--label">Course</h2>
-            <h3 className="course--title">{course.title}</h3>
-        </Link>
-    ));
-
-    return (
-        <div className="wrap main--grid">
-            {coursesList}
-            <Link
-                className="course--module course--add--module"
-                to={"/courses/create"}
-            >
-                <span className="course--add--title">
-                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                    viewBox="0 0 13 13" className="add">
-                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-                    </svg>
-                    New Course
-                </span>
-            </Link>
-        </div>
-    )
+    render() {
+        // Map over courses
+        const {courses} = this.state;
+        const coursesList =
+            courses.length > 0 ?
+            courses.map((course, index) => (
+                <Link
+                    to={`courses/${course.id}`}
+                    key={index}
+                    className="course--module course--link"
+                >
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">{course.title}</h3>
+                </Link>
+            )) : <h2>Loading...</h2>;
+    
+        return (
+            <div className="wrap main--grid">
+                {coursesList}
+                <Link
+                    className="course--module course--add--module"
+                    to={"/courses/create"}
+                >
+                    <span className="course--add--title">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" className="add">
+                            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                        </svg>
+                        New Course
+                    </span>
+                </Link>
+            </div>
+        )
+    }
 }
 
 export default Courses;
